25a8c652bfd012422bea76fab8efb3f79db015be
Ianukito <81605985+Ianuki@users.noreply.github.com>
2025-04-13 20:33:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,11 +9,18 @@ class vector2 {
 class line {
   constructor(a, b) { this.a = a; this.b = b; }
 }
+
+class triangle {
+  constructor(a, b, c) { this.a = a; this.b = b; this.c = c; }
+}
   
 function project(point, camera_position, camera_rotation) {
+  point = rotate_y(point, camera_rotation.j);
+  point = rotate_x(point, camera_rotation.i);
+
   return new vector2(
-    point.i * FOCAL_LENGTH / (point.k - camera_position.k),
-    point.j * FOCAL_LENGTH / (point.k - camera_position.k)  
+    (point.i * FOCAL_LENGTH) / (point.k - camera_position.k),
+    ((point.j - camera_position.j) * FOCAL_LENGTH) / (point.k - camera_position.k)  
   )
 }
   
@@ -55,21 +62,8 @@ let vertices = [
   new vector3(-1, -1, -1),
 ];
   
-const lines = [
-  new line(0, 1),
-  new line(0, 2),
-  new line(1, 3),
-  new line(3, 2),
-  new line(4, 5),
-  new line(4, 6),
-  new line(5, 7),
-  new line(7, 6),
-  new line(0, 4),
-  new line(1, 5),
-  new line(2, 6),
-  new line(3, 7),
-]
-  
+let triangles = []
+
 let camera_position = new vector3(0, 0, -10);
 let camera_rotation = new vector3(0, 0, 0);
   
@@ -82,29 +76,37 @@ let z_rotation = 0;
 const render = async () => { 
   context.clearRect(0, 0, CANVAS.width, CANVAS.height);
   
-  for (let i = 0; i < lines.length; i++) {
-    let point_a = vertices[lines[i].a]; point_a = rotate_x(point_a, x_rotation); point_a = rotate_y(point_a, y_rotation); point_a = rotate_z(point_a, z_rotation);
-    let point_b = vertices[lines[i].b]; point_b = rotate_x(point_b, x_rotation); point_b = rotate_y(point_b, y_rotation); point_b = rotate_z(point_b, z_rotation);
+  for (let i = 0; i < triangles.length; i++) {
+    let point_a = vertices[triangles[i].a];
+    let point_b = vertices[triangles[i].b];
+    let point_c = vertices[triangles[i].c];
     let projected_point_a = project(point_a, camera_position, camera_rotation);
     let projected_point_b = project(point_b, camera_position, camera_rotation);
+    let projected_point_c = project(point_c, camera_position, camera_rotation);
  
+    let path = new Path2D();
+    context.fillStyle = "rgba(255, 255, 255, 0.2)";
+    context.strokeStyle = "rgb(0, 255, 136)";
+    path.moveTo(projected_point_a.i + CANVAS.width / 2, projected_point_a.j + CANVAS.height / 2);
+    path.lineTo(projected_point_b.i + CANVAS.width / 2, projected_point_b.j + CANVAS.height / 2);
+    path.lineTo(projected_point_c.i + CANVAS.width / 2, projected_point_c.j + CANVAS.height / 2);
+    context.fill(path);
+
     context.beginPath()
-    context.strokeStyle = "white";
+    context.strokeStyle = "rgba(0, 255, 136, 0.2)";
     context.moveTo(projected_point_a.i + CANVAS.width / 2, projected_point_a.j + CANVAS.height / 2);
     context.lineTo(projected_point_b.i + CANVAS.width / 2, projected_point_b.j + CANVAS.height / 2);
+    context.lineTo(projected_point_c.i + CANVAS.width / 2, projected_point_c.j + CANVAS.height / 2);
     context.stroke();
   }
 
   for (let i = 0; i < vertices.length; i++) {
-    let point = vertices[i]; point = rotate_x(point, x_rotation); point = rotate_y(point, y_rotation); point = rotate_z(point, z_rotation);
+    let point = vertices[i];
     let projected_point = project(point, camera_position, camera_rotation);
 
     context.fillStyle = "rgb(0, 255, 136)";
-    context.fillRect(projected_point.i + CANVAS.width / 2 - 2.5, projected_point.j + CANVAS.height / 2 - 2.5, 5, 5);
+    context.fillRect(projected_point.i + CANVAS.width / 2, projected_point.j + CANVAS.height / 2, 1, 1);
   }
-
-  x_rotation += Math.PI / 180;
-  y_rotation += Math.PI / 360;
     
   requestAnimationFrame(render);
 }
@@ -124,7 +126,13 @@ document.getElementById("input").addEventListener("change", function (event) {
       character_array.push(String.fromCharCode(element));
     });
 
-    let file_contents_array = character_array.join("").split(" ");
+    let unfiltered_file_contents_array = character_array.join("").split(" ");
+    let file_contents_array = [];
+    unfiltered_file_contents_array.forEach(element => {
+      element.split("\n").forEach(_element => {
+        file_contents_array.push(_element);
+      });
+    });
     console.log(file_contents_array);
     
     for (let i = 0; i < file_contents_array.length; i++) {
@@ -141,9 +149,7 @@ document.getElementById("input").addEventListener("change", function (event) {
         B = file_contents_array[i + 2].split("/")[0] - 1;
         C = file_contents_array[i + 3].split("/")[0] - 1;
         
-        lines.push(new line(A, B));
-        lines.push(new line(B, C));
-        lines.push(new line(C, A));
+        triangles.push(new triangle(A, B, C));
       }
     }
 
@@ -159,18 +165,43 @@ window.onload = window.onresize = function() {
   canvas.height = window.innerHeight * 0.85;
 }
 
+let mouse_x = 0, mouse_y = 0;
+let mouse_down = 0;
+
+window.onmousedown = () => {
+  mouse_down = 1;
+}
+window.onmouseup = () => {
+  mouse_down = 0;
+}
+
+addEventListener("mousemove", (event) => {
+  if (mouse_down == 1) {
+    camera_rotation.j -= event.movementX * Math.PI / 360;
+    camera_rotation.i += event.movementY * Math.PI / 360;
+  }
+});
+
 window.addEventListener("keypress", function (e) { 
   e = e || window.event; 
   let char_code = e.keyCode || e.which; 
+  let char = String.fromCharCode(char_code);
   
-  if (char_code == 115) {
+  if (char == "s") {
     camera_position.k -= 1;
   }
-  else if (char_code == 119) {
+  else if (char == "w") {
     camera_position.k += 1;
+  }
+  else if (char == "q") {
+    camera_position.j -= 1;
+  }
+  else if (char == "e") {
+    camera_position.j += 1;
   }
 
 }, false);
+
 
   
 requestAnimationFrame(render);
